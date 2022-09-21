6db9b709e2bf4d9e9826b83840e6e2b53180843b
Nizomiddin-Xusniddinov <108681705+Nizomiddin-Xusniddinov@users.noreply.github.com>
2022-09-21 21:05:29 +0500

    added

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $(".fa-bars").click(function (){
+        $('.menushow').toggle(500).css({
+            'margin' : "auto"
+            
+        })
+    })
+
+});
